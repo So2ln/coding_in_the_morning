9213428c4a2f2c09f4075178368299dec65f0f2b
So2ln <sorrinj77@snu.ac.kr>
2025-06-30 09:47:56 +0900

    First commit

---
+++ bin/coding_in_the_morning.dart
@@ -0,0 +1,5 @@
+import 'package:coding_in_the_morning/coding_in_the_morning.dart' as coding_in_the_morning;
+
+void main(List<String> arguments) {
+  print('Hello world: ${coding_in_the_morning.calculate()}!');
+}
---
+++ test/coding_in_the_morning_test.dart
@@ -0,0 +1,8 @@
+import 'package:coding_in_the_morning/coding_in_the_morning.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
